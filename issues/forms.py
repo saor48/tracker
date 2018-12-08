3bd891e74e6d6004f2ddb42929523ef8d46e6b02
Saor <saor@ezspeak.eu>
2018-12-08 14:36:07 +0000

    fixed edit form

--- issues/forms.py
+++ issues/forms.py
@@ -1,14 +1,12 @@
 from django import forms
 from .models import Issue
-#ChoiceField(choices=CATEGORY_CHOICES )
+#ChoiceField(choices=CATEGORY_CHOICES)
 
 class CreateIssueForm(forms.ModelForm):
-    
-    CATEGORY_CHOICES = ['Bug', 'Feature'] 
-    
+    CATEGORY_CHOICES = [('bug', 'Bug'), ('feature', 'Feature')]
     name = forms.CharField(label='Name', max_length=254, required=True)
     description = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))
-    category = forms.CharField(label='Category', max_length=254, required=True)
+    category = forms.ChoiceField(label='Category', choices=CATEGORY_CHOICES, required=True)
     class Meta:
         model = Issue
         fields = ['name','description','category']
@@ -16,6 +14,7 @@ class CreateIssueForm(forms.ModelForm):
 
 class EditIssueForm(forms.ModelForm):
     id = forms.CharField(widget=forms.HiddenInput())
+    description = forms.CharField(label='Description', max_length=254, required=True)
     class Meta:
         model = Issue
         fields = ['id','name','description','comment','category','date_accepted','date_started','date_completed']
